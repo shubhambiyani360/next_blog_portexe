ba8d9b1f4e3d78036bd38034ae38e79eac497b1d
Shubham Biyani <shubhambiyani10@gmail.com>
2023-04-25 13:16:09 +0530

    auth token

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,6 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'vy0p2rlv',
-    dataset: 'production'
+    dataset: 'production',
+    apiversion: '2021-10-21',
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+    useCdn: true,
   }
 })
